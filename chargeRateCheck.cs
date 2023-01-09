3fb60bf692c4119b4cc65d9fa3df06f0fcc76298
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-09 18:13:21 +0530

    Update chargeRateCheck.cs

--- chargeRateCheck.cs
+++ chargeRateCheck.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Diagnostics;
 
-public static class chargeRateCheck
-{
-    public static bool chargeRateIsOk(float chargeRate)
+namespace NotUsed
+{   
+    public static class chargeRateCheck
     {
-         if(chargeRate > 0.8) 
-         {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-         }
-        return true;
+        public static bool chargeRateIsOk(float chargeRate)
+        {
+             if(chargeRate > 0.8) 
+             {
+                Console.WriteLine("Charge Rate is out of range!");
+                return false;
+             }
+            return true;
+        }
     }
 }
