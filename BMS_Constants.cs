870436f095d55c17b603b9ff6585509cc8c55503
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-01-09 18:05:13 +0530

    Create BMS_Constants.cs

---
+++ BMS_Constants.cs
@@ -0,0 +1,13 @@
+namespace SimpleMonitor
+{
+
+    public static class BMS_Constants
+    {
+        public const float socMin = 20f;
+        public const float socMax = 80f;
+        public const float tempMin = 0f;
+        public const float tempMax = 45f;
+        public const float chargeRateMax = 0.8f;
+        public const float Tolerance = 5f;
+    }
+}
